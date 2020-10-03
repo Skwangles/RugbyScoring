a309631c27a70c46c755b4cbeea1ff92b0ad3bdf
Skwangles <skwanglesavenue@gmail.com>
2020-10-03 14:38:45 +1300

    Added Files

---
+++ Rugby Scores/Rugby Scores/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rugby_Scores
+{
+    class Program
+    {
+        
+       
+
+        static void Main(string[] args)
+        {
+            Team A = new Team();
+            Team B = new Team();
+            Queue<string> events = new Queue<string>();
+            Console.WriteLine("Welcome to Rugby Scoring");
+
+            Console.Write("Enter Event: ");
+            string entry = Console.ReadLine();
+
+            while(entry != "X")
+            {
+                events.Enqueue(entry);
+                Console.Write("Enter Event: ");
+                entry = Console.ReadLine();
+
+            }
+        }
+
+       
+       
+
+
+
+    }
+}
---
+++ Rugby Scores/Rugby Scores/Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rugby_Scores
+{
+    class Team
+    {
+        private int conversionCount;
+        private int tryCount;
+        private int pointsModern;
+        private int pointsOld;
+        public Team()
+        {
+            pointsOld = 0;
+            pointsModern = 0;
+            tryCount = 0;
+           conversionCount = 0;
+        }
+
+        public void AddTry()
+        {
+            tryCount++;
+            pointsModern += 5;           
+        }
+
+        public void AddPenDrop()
+        {
+            pointsOld += 1;
+            pointsModern += 3;                
+        }
+
+        public void AddConversion()
+        {
+            pointsOld += 1;
+            pointsModern += 2;
+            conversionCount++;
+        }
+
+        public int Unconverted()
+        {
+            return (tryCount - conversionCount);
+        }
+    }
+}
